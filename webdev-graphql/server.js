ab81ab79d1dade9245d01b1346103e038cc1618e
Razy F <mr.razy.dev@gmail.com>
2024-04-23 00:43:49 +0300

    adding a books / author data objects and build a the functionality that return the books

--- webdev-graphql/server.js
+++ webdev-graphql/server.js
@@ -1,20 +1,58 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLInt,
+} from "graphql";
+import { books } from "./src/constants/index.js";
 const app = express();
-const schema = new GraphQLSchema({
+/* now we goona create a root query scope essentially this is just going to be the root query that everything is going to pull down from 
+here 👇 our root query is just this single helloWorld object and we can only query from this helloWorld object the messages field  */
+/* const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "HelloWorld",
-    fields: () => ({
-      /* now graphQl knows that our Hello World object has a message filed and that message filed is going to return a string */
+    fields: () => ({      
       message: {
-        type: GraphQLString,
-        /* lastely we needed to tell graphQl where to get this message from so in our case we just want to give it a static message of 'hello world' and this going to use the resolve section of this object which is just going to be a function that tells graphQl where to get the information from */
+        type: GraphQLString,        
         resolve: () => "Hello World",
       },
     }),
   }),
+}); */
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represent a book written by an author",
+  fields: () => ({
+    //we dont need to supply a resolve for our ID since we have an object that already has an ID property it will pull that ID property directly from that object
+    id: { type: GraphQLNonNull(GraphQLID) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+  }),
 });
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of books",
+      resolve: () => books,
+    },
+  }),
+});
+
+/* so now that we have our root query type which is using our book type we just need to create a schema and then use that schema  */
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
