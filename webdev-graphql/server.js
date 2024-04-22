f3514502f493df52ab8ad02ef6fda23114b8ffd2
Razy F <mr.razy.dev@gmail.com>
2024-04-23 01:12:27 +0300

    completing the process of fetching all books & authors and the link also books to authors and vice versa

--- webdev-graphql/server.js
+++ webdev-graphql/server.js
@@ -9,7 +9,7 @@ import {
   GraphQLNonNull,
   GraphQLInt,
 } from "graphql";
-import { books } from "./src/constants/index.js";
+import { authors, books } from "./src/constants/index.js";
 const app = express();
 /* now we goona create a root query scope essentially this is just going to be the root query that everything is going to pull down from 
 here 👇 our root query is just this single helloWorld object and we can only query from this helloWorld object the messages field  */
@@ -33,6 +33,29 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLNonNull(GraphQLID) },
     name: { type: GraphQLNonNull(GraphQLString) },
     authorId: { type: GraphQLNonNull(GraphQLInt) },
+    /* since our actual array of data doesnt have an author field we need to specify a custom resolve for how we get this author  */
+    author: {
+      type: AuthorType,
+      /* the resolve function takes as a first param a parent property which in our case is just going to be a book so this author is inside of this book type so this is going to get the book passed in the first param that is being queried for this field and we could check when this author ID is equal to the book author ID  */
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author of a book ",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLID) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
   }),
 });
 const RootQueryType = new GraphQLObjectType({
@@ -43,6 +66,11 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       description: "List of books",
       resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of Authors",
+      resolve: () => authors,
     },
   }),
 });
